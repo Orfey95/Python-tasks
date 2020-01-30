75436e50ce84352d01324bf4a8e31af692823893
Sasha <sasha7692@gmail.com>
2020-01-30 23:20:10 +0200

    Create Task_4_3.py

---
+++ Task4/Task_4_3.py
@@ -0,0 +1,37 @@
+import re
+import operator
+
+
+def count_user_agent(N):
+    log = open('C:\\Users\\Aleksandr\\Desktop\\EPAM\\Python\\access_log\\access_log')
+    first_regex_user_agent_list = []
+    second_regex_user_agent_list = []
+    line = log.readlines()
+    #  Get "..." from log
+    for x in range(len(line)):
+        first_regex_user_agent_list.append(re.findall(r'"(.*?)"', line[x]))
+    #  Convert list of "..." to string
+    str_user_agent_list = ''.join([str(elem) for elem in first_regex_user_agent_list])
+    #  Get .../d.d from list of "..."
+    second_regex_user_agent_list.append(re.findall(r' [a-zA-z]+/[0-9\.]+ ', str_user_agent_list))
+    second_regex_user_agent_list = second_regex_user_agent_list[0]
+    second_regex_user_agent_list = [element[1:-1] for element in second_regex_user_agent_list]
+    temp_dict = {}
+    for x in range(len(second_regex_user_agent_list)):
+        #  Reverse sort of user agents by their count
+        temp_dict = dict(sorted(temp_dict.items(), key=operator.itemgetter(1), reverse=True))
+        #  If length of dictionary is less than N, then fill out the dictionary
+        if len(temp_dict) < N:
+            temp_dict[second_regex_user_agent_list[x]] = second_regex_user_agent_list.count(second_regex_user_agent_list[x])
+        #  If length of dictionary is equal N, then replace user agent with the least number of repetitions with a new one
+        if len(temp_dict) == N and second_regex_user_agent_list.count(second_regex_user_agent_list[x]) > list(temp_dict.values())[N - 1]:
+            del temp_dict[list(temp_dict.keys())[list(temp_dict.values()).index(list(temp_dict.values())[N - 1])]]
+            temp_dict[second_regex_user_agent_list[x]] = second_regex_user_agent_list.count(second_regex_user_agent_list[x])
+    #  Reverse sort of user agents by their count
+    temp_dict = dict(sorted(temp_dict.items(), key=operator.itemgetter(1), reverse=True))
+    # Print dictionary line by line
+    for key, value in temp_dict.items():
+        print("%-12s %-25s %-10s %s" % ('User Agent:', key, 'Count: ', value))
+
+
+print(count_user_agent(7))
