70900d2fa689f26bcd18a4158a817a90f67013ea
Sasha <sasha7692@gmail.com>
2020-02-01 21:24:41 +0200

    Update Task_4_08.py

--- Task4/Task_4_08.py
+++ Task4/Task_4_08.py
@@ -6,18 +6,18 @@ def count_domain():
     log = open('C:\\Users\\Aleksandr\\Desktop\\EPAM\\Python\\access_log\\access_log')
     regex_domain_list = []
     line = log.readlines()
-    #  Get (...) from log
+    #  Get domain from log
     for x in range(len(line)):
         regex_domain_list.append(str((re.findall(r'"(?:http|https)://(.+?)(?:/|").+?"', line[x])))[2:-2])
     temp_dict = {}
     for x in range(len(regex_domain_list)):
         if regex_domain_list[x] != '':
             temp_dict[regex_domain_list[x]] = regex_domain_list.count(regex_domain_list[x])
-    #  Reverse sort of links by their count
+    #  Reverse sort of domains by their count
     temp_dict = dict(sorted(temp_dict.items(), key=operator.itemgetter(1), reverse=True))
     # Print dictionary line by line
     for key, value in temp_dict.items():
-        print('Count: ', value, 'URL: ', key)
+        print('Count: ', value, 'Domain: ', key)
 
 
 print(count_domain())
