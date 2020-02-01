a856a645e3f0ae798a53b6a4a558a41cb842eccd
Sasha <sasha7692@gmail.com>
2020-02-01 15:12:17 +0200

    Update Task_4_05.py

--- Task4/Task_4_05.py
+++ Task4/Task_4_05.py
@@ -4,39 +4,31 @@ import operator
 
 def count_max_and_min_link(N):
     log = open('C:\\Users\\Aleksandr\\Desktop\\EPAM\\Python\\access_log\\access_log')
-    first_regex_link_list = []
-    second_regex_link_list = []
+    regex_link_list = []
     line = log.readlines()
-    #  Get "GET ... HTTP/1.1" from log
+    #  Get /link from log
     for x in range(len(line)):
-        first_regex_link_list.append(re.findall(r'"GET (.+?) HTTP/1.1"', line[x]))
-    #  Convert list of "GET ... HTTP/1.1" to string
-    str_link_list = ' '.join([str(elem) for elem in first_regex_link_list])
-    #  Get "..." from list of "GET ... HTTP/1.1"
-    second_regex_link_list.append(re.findall(r' (.+?) ', str_link_list))
-    second_regex_link_list = second_regex_link_list[0]
-    #  Get ... from list of "..."
-    second_regex_link_list = [element[2:-2] for element in second_regex_link_list]
+        regex_link_list.append(str((re.findall(r'GET (.+? )HTTP/1.1', line[x])))[2:-3])
     max_len_dict = {}
     min_len_dict = {}
-    for x in range(len(second_regex_link_list)):
+    for x in range(len(regex_link_list)):
         #  Reverse sort of user agents by their count
         max_len_dict = dict(sorted(max_len_dict.items(), key=operator.itemgetter(1), reverse=True))
         min_len_dict = dict(sorted(min_len_dict.items(), key=operator.itemgetter(1), reverse=True))
         #  If length of dictionary is less than N, then fill out the dictionary
         if len(max_len_dict) < N:
-            max_len_dict[second_regex_link_list[x]] = len(second_regex_link_list[x])
+            max_len_dict[regex_link_list[x]] = len(regex_link_list[x])
         #  If length of dictionary is equal N, then replace link with the smallest length with a new one
-        if len(max_len_dict) == N and len(second_regex_link_list[x]) > list(max_len_dict.values())[N - 1]:
+        if len(max_len_dict) == N and len(regex_link_list[x]) > list(max_len_dict.values())[N - 1]:
             del max_len_dict[list(max_len_dict.keys())[list(max_len_dict.values()).index(list(max_len_dict.values())[N - 1])]]
-            max_len_dict[second_regex_link_list[x]] = len(second_regex_link_list[x])
+            max_len_dict[regex_link_list[x]] = len(regex_link_list[x])
         #  If length of dictionary is less than N, then fill out the dictionary
-        if len(min_len_dict) < N and len(second_regex_link_list[x]) >= 2:
-            min_len_dict[second_regex_link_list[x]] = len(second_regex_link_list[x])
+        if len(min_len_dict) < N and len(regex_link_list[x]) >= 2:
+            min_len_dict[regex_link_list[x]] = len(regex_link_list[x])
         #  If length of dictionary is equal N, then replace link with the longest length with a new one
-        if len(min_len_dict) == N and len(second_regex_link_list[x]) < list(min_len_dict.values())[0] and len(second_regex_link_list[x]) >= 2:
+        if len(min_len_dict) == N and len(regex_link_list[x]) < list(min_len_dict.values())[0] and len(regex_link_list[x]) >= 2:
             del min_len_dict[list(min_len_dict.keys())[list(min_len_dict.values()).index(list(min_len_dict.values())[0])]]
-            min_len_dict[second_regex_link_list[x]] = len(second_regex_link_list[x])
+            min_len_dict[regex_link_list[x]] = len(regex_link_list[x])
     #  Reverse sort of user agents by their count
     max_len_dict = dict(sorted(max_len_dict.items(), key=operator.itemgetter(1), reverse=True))
     min_len_dict = dict(sorted(min_len_dict.items(), key=operator.itemgetter(1)))
