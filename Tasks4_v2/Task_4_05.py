4794395011a9fff3a449c6fb78b00ee49f2ea903
Sasha <sasha7692@gmail.com>
2020-02-05 21:51:10 +0200

    Update Task_4_05.py

--- Tasks4_v2/Task_4_05.py
+++ Tasks4_v2/Task_4_05.py
@@ -1,44 +1,47 @@
 import re
-import operator
+from collections import Counter
 
 
 def count_max_and_min_link(N):
-    log = open('access_log')
+    log = open('C:\\Users\\Aleksandr\\Desktop\\EPAM\\Python\\access_log\\access_log')
     regex_link_list = []
     line = log.readlines()
     #  Get /link from log
     for x in range(len(line)):
-        regex_link_list.append(str((re.findall(r"(?:GET|POST) (.+?) HTTP/1.1", line[x])))[2:-2])
-    max_len_dict = {}
-    min_len_dict = {}
-    for x in range(len(regex_link_list)):
-        #  Reverse sort of links by their count
-        max_len_dict = dict(sorted(max_len_dict.items(), key=operator.itemgetter(1), reverse=True))
-        min_len_dict = dict(sorted(min_len_dict.items(), key=operator.itemgetter(1), reverse=True))
-        #  If length of dictionary is less than N, then fill out the dictionary
-        if len(max_len_dict) < N:
-            max_len_dict[regex_link_list[x]] = len(regex_link_list[x])
-        #  If length of dictionary is equal N, then replace link with the smallest length with a new one
-        if len(max_len_dict) == N and len(regex_link_list[x]) > list(max_len_dict.values())[N - 1]:
-            del max_len_dict[list(max_len_dict.keys())[list(max_len_dict.values()).index(list(max_len_dict.values())[N - 1])]]
-            max_len_dict[regex_link_list[x]] = len(regex_link_list[x])
-        #  If length of dictionary is less than N, then fill out the dictionary
-        if len(min_len_dict) < N and len(regex_link_list[x]) >= 1:
-            min_len_dict[regex_link_list[x]] = len(regex_link_list[x])
-        #  If length of dictionary is equal N, then replace link with the longest length with a new one
-        if len(min_len_dict) == N and len(regex_link_list[x]) < list(min_len_dict.values())[0] and len(regex_link_list[x]) >= 1:
-            del min_len_dict[list(min_len_dict.keys())[list(min_len_dict.values()).index(list(min_len_dict.values())[0])]]
-            min_len_dict[regex_link_list[x]] = len(regex_link_list[x])
-    #  Reverse sort of links by their count
-    max_len_dict = dict(sorted(max_len_dict.items(), key=operator.itemgetter(1), reverse=True))
-    min_len_dict = dict(sorted(min_len_dict.items(), key=operator.itemgetter(1)))
-    # Print dictionary line by line
-    print("The list of links with the maximum length:")
-    for key, value in max_len_dict.items():
-        print("%-12s %-25s %-10s %s" % ('Length:', value, 'URL:', key))
+        if len(str(re.findall(r"(?:GET|POST) (.+?) HTTP/1.1", line[x]))[2:-2]) >= 2:
+            regex_link_list.append(str(re.findall(r"(?:GET|POST) (.+?) HTTP/1.1", line[x]))[2:-2])
+    #  Counter for links
+    count_link_dict = Counter(regex_link_list)
+    new_d = {}
+    new_d2 = {}
+    for k in sorted(count_link_dict, key=len):
+        new_d[k] = count_link_dict[k]
+    # Get first N items in dictionary
     print("The list of links with the minimum length:")
-    for key, value in min_len_dict.items():
-        print("%-12s %-25s %-10s %s" % ('Length:', value, 'URL:', key))
+    out = dict(list(new_d.items())[0: N])
+    if N <= len(new_d):
+        # Print dictionary
+        for key, value in out.items():
+            print("%-10s %-10s %-10s %-5s %-10s %s" % ('Length: ', len(key), 'Count: ', value, 'URL: ', key))
+    else:
+        N = len(new_d)
+        # Print dictionary
+        for key, value in out.items():
+            print("%-10s %-10s %-10s %-5s %-10s %s" % ('Length: ', len(key), 'Count: ', value, 'URL: ', key))
+    for k in sorted(count_link_dict, key=len, reverse=True):
+        new_d2[k] = count_link_dict[k]
+    # Get first N items in dictionary
+    print("The list of links with the maximum length:")
+    out2 = dict(list(new_d2.items())[0: N])
+    if N <= len(new_d2):
+        # Print dictionary
+        for key, value in out2.items():
+            print("%-10s %-10s %-10s %-5s %-10s %s" % ('Length: ', len(key), 'Count: ', value, 'URL: ', key))
+    else:
+        N = len(new_d2)
+        # Print dictionary
+        for key, value in out2.items():
+            print("%-10s %-10s %-10s %-5s %-10s %s" % ('Length: ', len(key), 'Count: ', value, 'URL: ', key))
 
 
 print(count_max_and_min_link(7))
