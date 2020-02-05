777ce79de8ae265d225521582a52ff0d8130b2e8
Sasha <sasha7692@gmail.com>
2020-02-05 12:01:01 +0200

    Add files via upload

---
+++ Tasks4_v2/maser_call.py
@@ -0,0 +1,31 @@
+import Task_4_01
+import Task_4_02
+import Task_4_03
+import Task_4_04
+import Task_4_05
+import Task_4_06
+import Task_4_07
+import Task_4_08
+import Task_4_09
+import Task_4_10
+
+print("TASK 1")
+print(Task_4_01.count_ip(7))
+print("TASK 2")
+print(Task_4_02.request_frequency(4))
+print("TASK 3")
+print(Task_4_03.count_user_agent(7))
+print("TASK 4")
+print(Task_4_04.error_code_frequency(5))
+print("TASK 5")
+print(Task_4_05.count_max_and_min_link(7))
+print("TASK 6")
+print(Task_4_06.count_max_link_to_slash(4, 4))
+print("TASK 7")
+print(Task_4_07.count_upstream())
+print("TASK 8")
+print(Task_4_08.count_domain())
+print("TASK 9")
+print(Task_4_09.upstream_frequency())
+print("TASK 10")
+print(Task_4_10.max_request_frequency(5))
