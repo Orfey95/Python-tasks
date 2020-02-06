0f77e9c6aadd2ae90c7c33bc443ac5d7852f6639
Sasha <sasha7692@gmail.com>
2020-02-06 11:01:07 +0200

    Update Task_4_01.py

--- Tasks4_v2/Task_4_01.py
+++ Tasks4_v2/Task_4_01.py
@@ -4,19 +4,13 @@ from collections import Counter
 
 def count_ip(N):
     log = open('access_log')
-    first_regex_ip_list = []
-    second_regex_ip_list = []
+    regex_ip_list = []
     line = log.readlines()
-    #  Get (...) from log
+    #  Get ip address from log
     for x in range(len(line)):
-        first_regex_ip_list.append(re.findall(r'\([^"]+\)', line[x]))
-    #  Convert list of (...) to string
-    str_ip_list = ''.join([str(elem) for elem in first_regex_ip_list])
-    #  Get d.d.d.d from list of (...)
-    second_regex_ip_list.append(re.findall(r'\d+\.\d+\.\d+\.\d+', str_ip_list))
-    second_regex_ip_list = second_regex_ip_list[0]
+        regex_ip_list.append(str((re.findall(r'\((\d+\.\d+\.\d+\.\d+)', line[x])))[2:-2])
     #  Counter for ip addresses
-    temp_dict = Counter(second_regex_ip_list)
+    temp_dict = Counter(regex_ip_list)
     # Print dictionary line by line
     for key, value in temp_dict.most_common(N):
         print('Count: ', value, 'IP address: ', key)
