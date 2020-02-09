baeb664feff286a3a1f452f927ebf19e23aacb97
Sasha <sasha7692@gmail.com>
2020-02-09 23:24:47 +0200

    Update Task5.py

--- Task5/Task5.py
+++ Task5/Task5.py
@@ -4,7 +4,7 @@ import re
 
 
 __ERROR_ARGUMENT = 2
-__SUCCESSFUL = 1
+__SUCCESSFUL = 0
 
 
 def read_CSV(file_name, f_head):
@@ -44,11 +44,30 @@ def filter_csv(csv_body, column_regex, filter_regex):
                 csv_body.remove(csv_body_element)
 
 
+def manual():
+    print('DESCRIPTION')
+    print('\tA program for filtering csv files.\n')
+    print('\t-f, --file <input_file> \n\t specifying the path to the csv file')
+    print('\t-h, --help \n\t get help about the program')
+    print('\t-H, --head \n\t specify this parameter if the csv file contains a header')
+    print('\t-c, --col <col_number> \n\t specify the column number to which you want to apply a filter')
+    print('\t-F, --filter <filter_regex> \n\t specify a filtering rule, example: *KH*')
+    print('\t-o, --out <output_file> \n\t specify the path where the csv file will be written')
+    print('\n\tThe -c and -F options work only together, and cannot be alone.\n')
+    print('  Exit status:')
+    print('\t0\t-\tif OK,')
+    print('\t1\t-\tif critical error,')
+    print('\t2\t-\tif problem with arguments.')
+    print('\nAUTHOR')
+    print('\tWritten by Oleksandr Frolov.')
+
+
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "hf:Ho:c:F:", ["help="])
+        opts, args = getopt.getopt(argv, "hf:Ho:c:F:", ["help", "file=", "head", "out=", "col=", "filter="])
     except getopt.GetoptError:
-        print("demo_parse.py -f <file_name>")
+        print("An error occurred while specifying program parameters. "
+              "To specify the correctness, specify the -h or --help option")
         sys.exit(__ERROR_ARGUMENT)
     print(opts)
     csv_opt = dict()
@@ -56,30 +75,33 @@ def main(argv):
     csv_opt["column_regex"] = list()
     csv_opt["filter_regex"] = list()
     csv_opt["check_filter"] = False
+    csv_opt["check_read"] = False
     csv_opt["check_write"] = False
     for opt, arg in opts:
-        if opt == ('-h', "--help"):
-            print('demo_getopt.py.py -f | --file <input_file>')
-            print('demo_getopt.py.py -h | -- help')
+        if opt in ('-h', "--help"):
+            manual()
             sys.exit(__SUCCESSFUL)
-        elif opt in ("-f"):
+        elif opt in ("-f", "--file"):
+            csv_opt["check_read"] = True
             csv_opt["file"] = arg
-        elif opt in ("-H"):
+        elif opt in ("-H", "--head"):
             csv_opt["head"] = 0
-        elif opt in ("-o"):
+        elif opt in ("-o", "--out"):
             csv_opt["output_file"] = arg
             csv_opt["check_write"] = True
-        elif opt in ("-c"):
+        elif opt in ("-c", "--col"):
             csv_opt["column_regex"].append(arg)
             csv_opt["check_filter"] = True
-        elif opt in ("-F"):
+        elif opt in ("-F", "--filter"):
             csv_opt["filter_regex"].append(arg)
 
-    csv_head, csv_body = read_CSV(csv_opt["file"], csv_opt["head"])
+    if csv_opt["check_read"]:
+        csv_head, csv_body = read_CSV(csv_opt["file"], csv_opt["head"])
     if csv_opt["check_filter"]:
         filter_csv(csv_body, csv_opt["column_regex"], csv_opt["filter_regex"])
-    print(*csv_head, sep="\n")
-    print(*csv_body, sep="\n")
+    if csv_opt["check_read"] or csv_opt["check_filter"]:
+        print(*csv_head, sep="\n")
+        print(*csv_body, sep="\n")
     if csv_opt["check_write"]:
         write_csv(csv_opt["output_file"], csv_head, csv_body)
 
