cdb6a480692db65bbb9438a66796bf6d48895ca0
Sasha <sasha7692@gmail.com>
2020-02-10 15:48:47 +0200

    Update Task5.py

--- Task5/Task5.py
+++ Task5/Task5.py
@@ -5,20 +5,19 @@ import re
 
 __ERROR_ARGUMENT = 2
 __SUCCESSFUL = 0
-
+__COL_NUMBERS = 12  # number of columns of csv file
 
 def read_CSV(file_name, f_head):
     csv_head = list()
     csv_body = list()
-    col_number = 12  # number of columns of csv file
     start_element_of_list_app = list()
     end_element_of_list_app = list()
     with open(file_name, "r") as record:
-        if f_head == 1:
+        if f_head:
             csv_head.append(record.readline().replace("\n", "").split(","))
         for line in record:
             while True:
-                if len(line.split(',')) < col_number:
+                if len(line.split(',')) < __COL_NUMBERS:
                     line = str(line).replace("\n", "") + next(record).replace("\n", "")
                 else:
                     break
@@ -51,15 +50,43 @@ def filter_csv(csv_body, column_regex, filter_regex):
                 csv_body.remove(csv_body_element)
 
 
+
+def range_rows(csv_body, range_rows):
+    matching_rows = list()
+    for range_rows_element in range_rows:
+        start_number = int(str(range_rows_element).split("-")[0])
+        end_number = int(str(range_rows_element).split("-")[1])
+        for x in range(start_number, end_number+1):
+            matching_rows.append(x)
+    for index, csv_body_element in enumerate(csv_body[:]):
+        if index not in matching_rows:
+            csv_body.remove(csv_body_element)
+
+
+def range_columns(csv_head, csv_body, range_columns, f_head):
+    matching_columns = list()
+    for range_columns_element in range_columns:
+        start_number = int(str(range_columns_element).split("-")[0])
+        end_number = int(str(range_columns_element).split("-")[1])
+        for x in range(start_number, end_number+1):
+            matching_columns.append(x)
+    for csv_body_element in csv_body[:]:
+        del csv_body_element[matching_columns[0]:matching_columns[-1]+1]
+    if f_head:
+        del csv_head[0][matching_columns[0]:matching_columns[-1]]
+
+
 def manual():
     print('DESCRIPTION')
     print('\tA program for filtering csv files.\n')
-    print('\t-f, --file <input_file> \n\t specifying the path to the csv file')
-    print('\t-h, --help \n\t get help about the program')
-    print('\t-H, --head \n\t specify this parameter if the csv file contains a header')
-    print('\t-c, --col <col_number> \n\t specify the column number to which you want to apply a filter')
-    print('\t-F, --filter <filter_regex> \n\t specify a filtering rule, example: *KH*')
-    print('\t-o, --out <output_file> \n\t specify the path where the csv file will be written')
+    print('\t-f, --file <input_file> \n\t\t specifying the path to the csv file')
+    print('\t-h, --help \n\t\t get help about the program')
+    print('\t-H, --head \n\t\t specify this parameter if the csv file contains a header')
+    print('\t-c, --col <col_number> \n\t\t specify the column number to which you want to apply a filter')
+    print('\t-F, --filter <filter_regex> \n\t\t specify a filtering rule, example: -F *KH*')
+    print('\t-o, --out <output_file> \n\t\t specify the path where the csv file will be written')
+    print('\t-r, --row_range <range> \n\t\t specify the range of rows you need, example: -r 1-3')
+    print('\t-C, --column_range <range> \n\t\t specify the range of columns you need, example: -C 1-3')
     print('\n\tThe -c and -F options work only together, and cannot be alone.\n')
     print('  Exit status:')
     print('\t0\t-\tif OK,')
@@ -71,19 +98,23 @@ def manual():
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "hf:Ho:c:F:", ["help", "file=", "head", "out=", "col=", "filter="])
+        opts, args = getopt.getopt(argv, "hf:Ho:c:F:r:C:", ["help", "file=", "head", "out=", "col=", "filter=", "row_range=", "column_range="])
     except getopt.GetoptError:
         print("An error occurred while specifying program parameters. "
               "To specify the correctness, specify the -h or --help option")
         sys.exit(__ERROR_ARGUMENT)
     # print(opts)
     csv_opt = dict()
-    csv_opt["head"] = 1
+    csv_opt["head"] = True
     csv_opt["column_regex"] = list()
     csv_opt["filter_regex"] = list()
+    csv_opt["range_rows"] = list()
+    csv_opt["range_columns"] = list()
     csv_opt["check_filter"] = False
     csv_opt["check_read"] = False
     csv_opt["check_write"] = False
+    csv_opt["check_range_row"] = False
+    csv_opt["check_range_column"] = False
     for opt, arg in opts:
         if opt in ('-h', "--help"):
             manual()
@@ -92,7 +123,7 @@ def main(argv):
             csv_opt["check_read"] = True
             csv_opt["file"] = arg
         elif opt in ("-H", "--head"):
-            csv_opt["head"] = 0
+            csv_opt["head"] = False
         elif opt in ("-o", "--out"):
             csv_opt["output_file"] = arg
             csv_opt["check_write"] = True
@@ -101,9 +132,19 @@ def main(argv):
             csv_opt["check_filter"] = True
         elif opt in ("-F", "--filter"):
             csv_opt["filter_regex"].append(arg)
+        elif opt in ("-r", "--row_range"):
+            csv_opt["range_rows"].append(arg)
+            csv_opt["check_range_row"] = True
+        elif opt in ("-C", "--column_range"):
+            csv_opt["range_columns"].append(arg)
+            csv_opt["check_range_column"] = True
 
     if csv_opt["check_read"]:
         csv_head, csv_body = read_CSV(csv_opt["file"], csv_opt["head"])
+    if csv_opt["check_range_row"]:
+        range_rows(csv_body, csv_opt["range_rows"])
+    if csv_opt["check_range_column"]:
+        range_columns(csv_head, csv_body, csv_opt["range_columns"], csv_opt["head"])
     if csv_opt["check_filter"]:
         filter_csv(csv_body, csv_opt["column_regex"], csv_opt["filter_regex"])
     if csv_opt["check_read"] or csv_opt["check_filter"]:
